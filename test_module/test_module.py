909e65d2368efc44474b12221a7d9a391d418ba3
Somov Ruslan <reslic52@gmail.com>
2021-12-20 23:29:43 +0300

    - Внесены правки

--- test_module/test_module.py
+++ test_module/test_module.py
@@ -1,3 +1,6 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge
 
 
 def test_module(data_in: int, data_out: list):
@@ -8,6 +11,7 @@ def test_module(data_in: int, data_out: list):
         if data == data_in:
             index = data_out_into.index(data) + 1
     #print ("depth", index)
+    temp_ = 0
     for i in range(index):
         temp = data_out_into[i]
         if (i == 0):
@@ -22,6 +26,7 @@ def test_module(data_in: int, data_out: list):
 
 data_in_tamplate = [[1, 2, 1, 2, 1, 2, 1], [1, 2, 3, 4, 3, 2, 3, 4, 3, 4, 4]]
 
+
 def test_module_tb():
     for template in data_in_tamplate:
         data_reset = [0, 0, 0, 0]
@@ -35,4 +40,32 @@ def test_module_tb():
         test_cnt = test_cnt + 1
 
 
-test_module_tb()
+# test_module_tb()
+
+async def reset(dut):
+    dut.reset_in.setimmediatevalue(0)
+    await RisingEdge(dut.clk_in)
+    await RisingEdge(dut.clk_in)
+    dut.reset_in <= 1
+    await RisingEdge(dut.clk_in)
+    await RisingEdge(dut.clk_in)
+    dut.reset_in <= 0
+
+
+@cocotb.test()
+async def test_module(dut):
+    """Tr accessign the design."""
+    cocotb.start_soon(Clock(dut.clk_in, 8, units="ns").start())
+    await reset(dut)
+    for template in data_in_tamplate:
+        data_reset = [0, 0, 0, 0]
+        # data_out = test_module(0, data_reset)
+        test_cnt = 0
+        print("tests: ", test_cnt)
+        for data_in in template:
+            print("# test_cnt")
+            # data_out = test_module(data_in, data_out)
+            dut.data_in <= data_in
+            await RisingEdge(dut.clk_in)
+            # print(data_out)
+        test_cnt = test_cnt + 1
--- test_module/test_module.sv
+++ test_module/test_module.sv
@@ -1,9 +1,9 @@
 module test_module #(
     parameter DATA_W = 8
 )(
-    input   logic                       clk_in,
-    input   logic                       reset_in,
-    input   logic [(DATA_W - 1) : 0]    data_in,
+    input  logic                        clk_in,
+    input  logic                        reset_in,
+    input  logic [(DATA_W - 1) : 0]     data_in,
 
     output logic [(DATA_W - 1) : 0]     out_0,
     output logic                        out_valid_0,
@@ -25,37 +25,46 @@ module test_module #(
     // Work signals
     logic [3:0]                 out_valid;
     logic [3:0][DATA_W-1:0]     out_data;
-    logic [3:0]                 catch;
+    logic [3:0]                 catching;
 
     // Assigns
     assign out_0 = out_data[0];
     assign out_1 = out_data[1];
     assign out_2 = out_data[2];
     assign out_3 = out_data[3];
+    assign out_valid_0 = out_valid[0];
+    assign out_valid_1 = out_valid[1];
+    assign out_valid_2 = out_valid[2];
+    assign out_valid_3 = out_valid[3];
 
-    //
+    // Set up if equal input data or less if eqaul input data
     always_comb begin
 
         for (int i = 3; i >= 0; i--) begin
-            if (out_data[i] == data_in || (i < 3 && catch[i + 1] == 1'b1)) begin
-                catch[i] = 1'b1;
+            if (out_data[i] == data_in || (i < 3 && catching[i + 1] == 1'b1)) begin
+                catching[i] = 1'b1;
             end else begin
-                catch[i] = 1'b0;
+                catching[i] = 1'b0;
             end
+            // if(out_data[i] == data_in) begin
+            //     catching[i] = 1'b1;
+            // end else begin
+            //     catching[i] = 1'b0;
+            // end
         end
 
     end
 
 
-    //
+    // Form output signals and data
     always_ff @(posedge clk_in) begin
         if (reset_in == 1'b1) begin
             out_valid <= '{default:'0};
             out_data <= '{default:'0};
         end else begin
 
-            for (int i = 0; i < 3; i++) begin
-                if (catch[i] == 1'b1) begin
+            for (int i = 0; i < 4; i++) begin
+                if (catching[i] == 1'b1 || |catching == 1'b0) begin
                     if (i == 0) begin
                         out_data[i] <= data_in;
                     end else begin
@@ -64,7 +73,15 @@ module test_module #(
                 end
             end
 
-            out_valid <= out_valid | catch;
+            for (int i = 0; i < 4; i++) begin
+                if (catching[i] == 1'b1 || |catching == 1'b0) begin
+                    if (i == 0) begin
+                        out_valid[i] <= 1'b1;
+                    end else begin
+                        out_valid[i] <= out_valid[i - 1];
+                    end
+                end
+            end
 
         end
     end
